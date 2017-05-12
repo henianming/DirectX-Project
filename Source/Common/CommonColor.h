81188bc5e6c762f367760a5f0abe96380cf6342d
henianming <1500923374@qq.com>
2017-05-12 13:08:00 +0800

    颜色

---
+++ Source/Common/CommonColor.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <d3d9types.h>
+
+//WEB COLOR
+#define HCOLOR_ALICEBLUE D3DCOLOR_XRGB(240, 248, 255) //爱丽丝蓝
+#define HCOLOR_AntiqueWhite D3DCOLOR_XRGB(250, 235, 215) //古董白
+#define HCOLOR_AquaMarine D3DCOLOR_XRGB(127, 255, 212) //碧绿
+#define HCOLOR_Azure D3DCOLOR_XRGB(240, 255, 255) //青白色
+#define HCOLOR_Beige D3DCOLOR_XRGB(245, 245, 220) //米色
+#define HCOLOR_Bisque D3DCOLOR_XRGB(255, 228, 196) //陶坯黄
+#define HCOLOR_Black D3DCOLOR_XRGB(0, 0, 0) //黑色
+#define HCOLOR_BlanchedAlmond D3DCOLOR_XRGB(255, 235, 205) //杏仁白
+#define HCOLOR_Blue D3DCOLOR_XRGB(0, 0, 255) //蓝色
+#define HCOLOR_BlueViolet D3DCOLOR_XRGB(138, 43, 226) //蓝紫色
+#define HCOLOR_Brown D3DCOLOR_XRGB(165, 42, 42) //褐色
+#define HCOLOR_BurlyWood D3DCOLOR_XRGB(222, 184, 135) //硬木褐
+#define HCOLOR_CadetBlue D3DCOLOR_XRGB(95, 158, 160) //军服蓝
+#define HCOLOR_ChartReuse D3DCOLOR_XRGB(127, 255, 0) //查特酒绿
+#define HCOLOR_Chocolate D3DCOLOR_XRGB(210, 105, 30) //巧克力色
+#define HCOLOR_Coral D3DCOLOR_XRGB(255, 127, 80) //珊瑚红
+#define HCOLOR_CornFlowerBlue D3DCOLOR_XRGB(100, 149, 237) //矢车菊蓝
+#define HCOLOR_CornSilk D3DCOLOR_XRGB(255, 248, 220) //玉米穗黄
+#define HCOLOR_Crimson D3DCOLOR_XRGB(220,20,60) //绯红
+#define HCOLOR_Cyan D3DCOLOR_XRGB(0, 255, 255) //青色
+#define HCOLOR_DarkBlue D3DCOLOR_XRGB(0, 0, 139) //深蓝
+#define HCOLOR_DarkCyan D3DCOLOR_XRGB(0, 139, 139) //深青
+#define HCOLOR_DarkGoldenRod D3DCOLOR_XRGB(184, 134, 11) //深金菊黄
+#define HCOLOR_DarkGray D3DCOLOR_XRGB(169, 169, 169) //暗灰
+#define HCOLOR_DarkGreen D3DCOLOR_XRGB(0, 100, 0) //深绿
+#define HCOLOR_DarkKhaki D3DCOLOR_XRGB(189, 183, 107) //深卡其色
+#define HCOLOR_DarkMagenta D3DCOLOR_XRGB(139, 0, 139) //深品红
+#define HCOLOR_DarkOliveGreen D3DCOLOR_XRGB(85, 107, 47) //深橄榄绿
+#define HCOLOR_DarkOrange D3DCOLOR_XRGB(255, 140, 0) //深橙
+#define HCOLOR_DarkOrchid D3DCOLOR_XRGB(153, 50, 204) //深洋兰紫
+#define HCOLOR_DarkRed D3DCOLOR_XRGB(139, 0, 0) //深红
+#define HCOLOR_DarkSalmon D3DCOLOR_XRGB(233, 150, 122) //深鲑红
+#define HCOLOR_DarkSeaGreen D3DCOLOR_XRGB(143, 188, 143) //深海藻绿
+#define HCOLOR_DarkSlateBlue D3DCOLOR_XRGB(72, 61, 139) //深岩蓝
+#define HCOLOR_DarkSlateGray D3DCOLOR_XRGB(47,79,79) //深岩灰
+#define HCOLOR_DarkTurquoise D3DCOLOR_XRGB(0, 206, 209) //深松石绿
+#define HCOLOR_DarkViolet D3DCOLOR_XRGB(148, 0, 211) //深紫
+#define HCOLOR_DeepPink D3DCOLOR_XRGB(255, 20, 147) //深粉
+#define HCOLOR_DeepSkyBlue D3DCOLOR_XRGB(0, 191, 255) //深天蓝
+#define HCOLOR_DimGray D3DCOLOR_XRGB(105, 105, 105) //昏灰
+#define HCOLOR_DodgerBlue D3DCOLOR_XRGB(30, 144, 255) //湖蓝
+#define HCOLOR_FireBrick D3DCOLOR_XRGB(178, 34, 34) //火砖红
+#define HCOLOR_FloralWhite D3DCOLOR_XRGB(255, 250, 240) //花卉白
+#define HCOLOR_ForestGreen D3DCOLOR_XRGB(34, 139, 34) //森林绿
+#define HCOLOR_GainsBoro D3DCOLOR_XRGB(220, 220, 220) //庚氏灰
+#define HCOLOR_GhostWhite D3DCOLOR_XRGB(248, 248, 255) //幽灵白
